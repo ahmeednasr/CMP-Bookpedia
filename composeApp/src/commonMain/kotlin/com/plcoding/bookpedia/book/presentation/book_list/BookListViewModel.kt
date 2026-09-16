481a57c09c9b6bc6379b43bf74a954d4dd1b48c1
Nasr <ahmednasr159@outlook.com>
2026-09-17 01:00:37 +0300

    init BookScreenList,SearchBar

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListViewModel.kt
@@ -3,6 +3,7 @@ package com.plcoding.bookpedia.book.presentation.book_list
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class BookListViewModel : ViewModel(){
     private val _state= MutableStateFlow(BookListState())
@@ -10,10 +11,20 @@ class BookListViewModel : ViewModel(){
 
     fun onAction(action: BookListAction){
         when(action){
-            is BookListAction.OnBookClick -> TODO()
-            is BookListAction.OnSearchQueryChange -> TODO()
-            is BookListAction.OnTabSelected -> TODO()
+            is BookListAction.OnBookClick ->  {
+
+            }
+            is BookListAction.OnSearchQueryChange -> {
+                _state.update {
+                    it.copy(searchQuery = action.query)
+                }
+            }
+            is BookListAction.OnTabSelected -> {
+                _state.update {
+                    it.copy(selectedTabIndex = action.index)
+                }
+            }
         }
     }
-   // 35 minutes in video
+   // 41 minutes in video
 }
