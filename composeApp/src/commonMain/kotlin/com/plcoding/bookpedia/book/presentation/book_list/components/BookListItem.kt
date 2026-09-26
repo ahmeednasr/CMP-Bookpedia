349b54ea108474e97793e5589027cb68ef16e323
Ahmed Nasr <58734390+ahmeednasr@users.noreply.github.com>
2026-09-26 21:17:24 +0300

    Implement image loading and error handling in BookListItem

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
@@ -1,13 +1,16 @@
 package com.plcoding.bookpedia.book.presentation.book_list.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,10 +20,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.book_error_2
 import coil3.compose.rememberAsyncImagePainter
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.presentation.LightBlue
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun BookListItem(
@@ -47,7 +54,40 @@ fun BookListItem(
                 contentAlignment = Alignment.Center
             ) {
                 var imageLoadResult by remember { mutableStateOf<Result<Painter>?>(null) }
-                //val painter= rememberAsyncImagePainter()
+                val painter = rememberAsyncImagePainter(
+                    model = book.imageUrl,
+                    onSuccess = {
+                        val size = it.painter.intrinsicSize
+                        imageLoadResult = if (size.width > 1 && size.width > 1) {
+                            Result.success(it.painter)
+                        } else {
+                            Result.failure(Exception("Invalid image size"))
+                        }
+                    },
+                    onError = {
+                        it.result.throwable.printStackTrace()
+                        imageLoadResult = Result.failure(it.result.throwable)
+                    }
+                )
+                when (val result = imageLoadResult) {
+                    null -> CircularProgressIndicator()
+                    else -> {
+                        Image(
+                            painter = if (result.isSuccess) painter
+                            else painterResource(Res.drawable.book_error_2)
+                            ,
+                            contentDescription = book.title,
+                            contentScale = if (result.isSuccess) ContentScale.Crop
+                            else ContentScale.Fit,
+                            modifier = Modifier.aspectRatio(
+                                ratio = 0.65f,
+                                matchHeightConstraintsFirst = true
+
+                            )
+                            )
+                    }
+                }
+
 
             }
         }
